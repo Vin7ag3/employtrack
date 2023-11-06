1be384d22ac3b009351567a4a055f3e4f053f951
Vintage <vincepeo@yahoo.com>
2023-11-06 01:12:46 -0500

    structure

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO department (name) VALUES ('Design'), ('Developer');
+
+INSERT INTO role (title, salary, department_id) VALUES
+  ('Lead Designer', 100000, 1),
+  ('Designer', 70000, 1),
+  ('Lead Developer', 130000, 2),
+  ('Developer', 100000, 2);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
+  ('Sola', 'Yu', 1, NULL),
+  ('Veasnar', 'Poe', 3, NULL),
+  ('Carlos', 'Cardona', 4, 2),
+  ('Doug', 'Jewel', 2, 1),
+  ('Kotori', 'Haibara', 2, 1),
+  ('Shohei', 'Kenei', 4, 2);
+
